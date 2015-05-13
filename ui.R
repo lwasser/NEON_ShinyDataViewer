5e781b8ec8916f774d8424f522d59572ec6b285f
Christine Laney <claney@neoninc.org>
2015-05-13 17:26:02 -0600

    general update

--- ui.R
+++ ui.R
@@ -3,19 +3,23 @@ library(shiny)
 library(scales)
 library(plyr)
 library(ggplot2)
-data <- read.csv('NEON.D10.CPER.DP1.00001.001.000.010.002.2DWSD_2min.csv')
 
 shinyUI(navbarPage(
   title = "NEON TIS Data",
+
   tabPanel("Wind Speed Histograms", 
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30),
-      plotOutput("minWSHist", height = "200px"),
-      plotOutput("meanWSHist", height = "200px"),
-      plotOutput("maxWSHist", height = "200px")
+           
+      sliderInput("binwidth",
+                  "Bin Width:",
+                  min = 0,
+                  max = 1,
+                  value = .5),
+      
+      selectInput('windvar',
+                  "Choose Variable",
+                  choices = names(data)[3:5]),
+  
+      plotOutput('histogram', height = "200px")
     )
   )
 )
