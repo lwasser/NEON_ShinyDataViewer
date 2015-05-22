47f7b80c752136c41f186d36cfdc126effbd85f5
Christine Laney <claney@neoninc.org>
2015-05-22 13:27:21 -0600

    Updated to view multiple TIS datasets.

--- server.R
+++ server.R
@@ -4,17 +4,26 @@ shinyServer(function(input, output) {
 
   datafiles <- list.files(path = 'data', pattern = 'min.csv')
   
-  makeTS <- reactive({
+  readTable <- reactive({
     data <- read.csv(input$datafile[1,4], header = TRUE, stringsAsFactors = FALSE)
-    #data <- read.csv(paste("data/", input$datafile, sep = ""), 
-    #                 header = TRUE, stringsAsFactors = FALSE)
+  })
+  
+  readVars <- reactive({
+    data <- readTable()
+    names <- names(data)
+  })
+  
+  makeTS <- reactive({
+    data <- readTable()
     data <- data[,-2]
     data[,1] <- as.POSIXct(substr(data[,1], 1, 19), format = "%Y-%m-%dT%H:%M:%S", tz = "GMT")
     tsdata <- xts(x = data[ , 2:ncol(data)], order.by = data[,1], tzone = "GMT")
   })
   
   makeHistogram <- reactive({
-    aes_mapping <- aes_string(x=input$windvar)
+    data <- readTable()
+    var <- as.character(input$histvar)
+    aes_mapping <- aes_string(x = var)
     g <- ggplot(data, mapping = aes_mapping) + 
       theme_bw() +
       geom_histogram(binwidth = input$binwidth)
@@ -23,30 +32,44 @@ shinyServer(function(input, output) {
  
 
   makeTimeSeriesChart <- reactive({
-    d <- data[,c("DateTime", input$windvar)]
+    d <- data[,c("DateTime", input$variables)]
     names(d) <- c("DateTime","var")
     g <- ggplot(d, aes(x=DateTime, y = var)) +
       theme_bw() +
       geom_line()
   })
   
+  output$datatable <- renderDataTable(readTable(),
+                                      options = list(pageLength = 10,
+                                                     scrollY = "500px",
+                                                     scrollX = "700px",
+                                                     scrollCollapse = FALSE,
+                                                     dom = 'lfrtip'))
+  
   output$histogram <- renderPlot({
     print(makeHistogram())
   })
+
+  output$tsvariables = renderUI({
+    selectizeInput('tsvars', 'Choose variable', readVars()[3:length(readVars())], 
+                   multiple = TRUE, width = "80%")
+  })
+  
+  output$histvars = renderUI({
+    selectInput('histvar', 'Choose variable', readVars()[3:length(readVars())])
+  })
   
   output$dygraph <- renderDygraph({
-    d <- makeTS()[,1:3]
+    d <- makeTS()[,c(input$tsvars)]
     minval <- min(d, na.rm = TRUE)
     maxval <- max(d, na.rm = TRUE)
     dygraph(d) %>%
       dyHighlight(highlightCircleSize = 5, 
                   highlightSeriesBackgroundAlpha = 0.2,
                   hideOnMouseOut = FALSE) %>%
-      dyAxis("y", label = "Wind Speed (m/s)", valueRange = c(minval, maxval * 1.5)) %>%
+      dyAxis("y", label = "", valueRange = c(minval, maxval * 1.2)) %>%
       dyRangeSelector()
   })
-  
-  #,highlightSeriesOpts = list(strokeWidth = 3)
-  
+
 })
 
--- ui.R
+++ ui.R
@@ -4,34 +4,33 @@ library(scales)
 library(plyr)
 library(ggplot2)
 library(reshape2)
-library(dygraphs)
 library(xts)
+library(dygraphs)
 
 shinyUI(navbarPage(
   title = "NEON TIS Data",
-
-    tabPanel("Wind Speed Time Series",
+  
+  tabPanel("Data",
            fileInput('datafile', "Select data file"),
-             
-           #selectInput('datafile',
-            #           'Select data file',
-             #          choices = datafiles,
-              #         width = '60%'),
-           br(),           
-           dygraphOutput("dygraph")
+           br(),
+           dataTableOutput('datatable')           
+           ),
+
+  tabPanel("Time Series Graph",
+           uiOutput('tsvariables'),
+           br(),
+           dygraphOutput("dygraph", height = "600px")
   ),
   
-  tabPanel("Wind Speed Histograms", 
+  tabPanel("Histograms", 
            
            sliderInput("binwidth",
                        "Bin Width:",
                         min = 0,
                         max = 1,
                         value = .5),
-            
-           selectInput('windvar',
-                       "Choose Variable",
-                        choices = names(data)[3:5]),
+           br(),
+           uiOutput('histvars'),
         
            plotOutput('histogram', height = "200px")
 
